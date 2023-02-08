4eb95561a267b55ca7583e485d1039031fa789ac
satoshicano <satoshicano@gmail.com>
2023-02-08 15:13:26 +0900

    format

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
+
 const app = express();
 
-app.set('port', process.env.PORT || 5050);
+app.set("port", process.env.PORT || 5050);
 
-app.listen(app.get('port'), () => console.log('Example app listening on port 5050!'));
+app.listen(app.get("port"), () =>
+  console.log("Example app listening on port 5050!")
+);
 
-app.get('/', (req, res) => res.send('ok'));
+app.get("/", (req, res) => res.send("ok"));
 
-app.post('/webhook', (req, res) => {
+app.post("/api", (req, res) => {
   console.log(req);
-  res.send("ok")
-})
+  res.send("ok");
+});
